9358aba950dbbdc5ab43a57feb44abd5bfd077a0
Marcus Ho Jw <marcushojww@gmail.com>
2021-11-17 23:21:23 +0800

    training done

---
+++ config.py
@@ -0,0 +1,20 @@
+# specify path to the flowers and mnist dataset
+MASK_DATASET_PATH = "dataset"
+
+# specify the paths to our training and validation set 
+TRAIN = "train"
+VAL = "val"
+TEST = "test"
+
+# set the input height and width
+INPUT_HEIGHT = 128
+INPUT_WIDTH = 128
+
+# set the validation data split
+VAL_SPLIT = 0.1
+TEST_SPLIT = 0.2
+
+# define training hyperparameters
+INIT_LR = 1e-3
+BATCH_SIZE = 64
+EPOCHS = 10
