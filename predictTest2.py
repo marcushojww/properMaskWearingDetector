45a0b120e73f9e87c62dd0b4969c6247ec011844
Marcus Ho Jw <marcushojww@gmail.com>
2021-12-05 21:43:53 +0800

    predict script

--- predictTest2.py
+++ predictTest2.py
@@ -23,43 +23,59 @@ import config
 # set the device we will be using to train the model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# load saved model
-model = torch.load('./LenetOutput/model.pth')
+print('Enter "g" for GoogLeNet, "r" for ResNet or "l" for LeNet to proceed: ')
 
-# initialize our data augmentation functions
-resize = transforms.Resize(size=(config.INPUT_HEIGHT,
- config.INPUT_WIDTH))
+inputModel = input()
 
-testTransforms = transforms.Compose([resize, transforms.ToTensor()])
+if (inputModel == 'g'):
+  model = torch.load('./GLNoutput/GLNmodel.pth')
+elif (inputModel == 'r'):
+  model = torch.load('./RNoutput/RNmodel.pth')
+elif (inputModel == 'l'):
+  model = torch.load('./LenetOutput/model.pth')
 
-test_folder = 'test_2'
 
-# initialize the training and validation dataset
-print("[INFO] loading test dataset...")
-testDataset = ImageFolder(root=test_folder, 
- transform=testTransforms)
+if (inputModel == 'l' or inputModel == 'g' or inputModel == 'r'):
+  # load saved model
+  model = torch.load('./LenetOutput/model.pth')
 
-print("[INFO] test dataset contains {} samples...".format(len(testDataset)))
+  # initialize our data augmentation functions
+  resize = transforms.Resize(size=(config.INPUT_HEIGHT,
+  config.INPUT_WIDTH))
 
-testDataLoader = DataLoader(testDataset, batch_size=config.BATCH_SIZE)
+  testTransforms = transforms.Compose([resize, transforms.ToTensor()])
 
-# turn off autograd for testing evaluation
-with torch.no_grad():
- # set the model in evaluation mode
- model.eval()
- 
- # initialize a list to store our predictions
- preds = []
+  test_folder = 'test_2'
 
- # loop over the test set
- for (input, label) in testDataLoader:
-  # send the input to the device
-  input = input.to(device)
+  # initialize the training and validation dataset
+  print("[INFO] loading test dataset...")
+  testDataset = ImageFolder(root=test_folder, 
+  transform=testTransforms)
 
-  # make the predictions and add them to the list
-  pred = model(input)
-  preds.extend(pred.argmax(axis=1).cpu().numpy())
-  
-# generate a classification report
-print(classification_report(np.array(testDataset.targets),
- np.array(preds), target_names=testDataset.classes))
+  print("[INFO] test dataset contains {} samples...".format(len(testDataset)))
+
+  testDataLoader = DataLoader(testDataset, batch_size=config.BATCH_SIZE)
+
+  # turn off autograd for testing evaluation
+  with torch.no_grad():
+    # set the model in evaluation mode
+    model.eval()
+
+    # initialize a list to store our predictions
+    preds = []
+
+    # loop over the test set
+    for (input, label) in testDataLoader:
+      # send the input to the device
+      input = input.to(device)
+
+      # make the predictions and add them to the list
+      pred = model(input)
+      preds.extend(pred.argmax(axis=1).cpu().numpy())
+
+  # generate a classification report
+  print(classification_report(np.array(testDataset.targets),
+  np.array(preds), target_names=testDataset.classes))
+
+else:
+  print('Invalid model input. Please run the script again.')
